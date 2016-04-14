e6aabda5202eda73878d262b66c2934aba67e327
Sebastiano Morando <sebastianomorando@gmail.com>
2016-04-15 00:31:06 +0200

    playing with socket.io

--- app.js
+++ app.js
@@ -13,11 +13,38 @@ server.listen(port, function () {
 app.use(express.static(__dirname + '/public'));
 
 var numUsers = 0;
+//here we need a global array to store all touches
+//redis?
+//when the array have more than 1000 touches, we pop the older ones
 
 io.on('connection', function (socket) {
   ++numUsers
+
+  socket.who = Math.random();
+
   console.log("Someone connected");
   console.log(numUsers+" connected users");
+
+  //everytime someone send his position information
+  //we broadcast all the position information to everyone
+  //or is maybe better to use a tick() and send every tot seconds
+  //the information?
+
+  //someone clicks, we receive the position
+  //we need some kind of global position
+  //or maybe the position can only be relative
+  //to the last position
+   socket.on('click', function (x,y) {
+      //we are going to calculate the new position
+     console.log(x+","+y);
+     //now i send the position to everyone
+     //the broadcast obviously doesn't have memory of past events
+      socket.broadcast.emit('draw', {
+        who : socket.who,
+        x: x,
+        y: y
+      });
+   });
 
 
    socket.on('disconnect', function () {
--- public/app.js
+++ public/app.js
@@ -4,4 +4,21 @@ function init() {
 
   var canvas = document.getElementById("canvas");
   var stage = new createjs.Stage(canvas);
+
+  stage.addEventListener("stagemouseup", function(e) {
+    socket.emit("click", e.stageX,e.stageY);
+    console.log("aaaa");
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("red").drawCircle(e.stageX,e.stageY,4);
+    stage.addChild(circle);
+    stage.update();
+  });
+
+  socket.on('draw', function (data) {
+    console.log(data);
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("red").drawCircle(data.x,data.y,4);
+    stage.addChild(circle);
+    stage.update();
+  });
 }
